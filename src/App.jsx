7685a759de641dfca7581e713b359db82b4e45c3
Jasoo73 <jsanchezosorio77@gmail.com>
2025-05-06 09:45:59 -0500

    Primera versión

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './componentes/myComponente';
+import AddCategory from './componentes/myCategoria';
+import GridGif from './componentes/myGridCategory';
+
+function App() {
+  //const [count, setCount] = useState(0)
+    <div>
+      <HelloWorld />
+      <AddCategory />
+    </div>
+}
+
+export default App;
